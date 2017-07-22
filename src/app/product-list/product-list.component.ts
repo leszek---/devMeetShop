c1a8630d403fd552b60acca6f1d539b5a87f7943
MSH <m.s.halas@gmail.com>
2017-07-22 15:22:49 +0200

    Sort buttons

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,5 +7,23 @@ import {Product} from '../../models/productModel';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-    @Input() products: Product[];
+  @Input() products: Product[];
+  
+  public filters: Array<{id:number, name:string, value:boolean}> = [
+    {id: 1, name: "Price", value: true},
+    {id: 2, name: "Name", value: false},
+    {id: 3, name: "Promoted", value: false}
+  ];
+
+  public onClick (filterToChange) {
+    this.filters.forEach(filter => { 
+      if(filter.id == filterToChange.id){
+        filter.value = true;
+      }else{
+        filter.value = false;
+      }
+    });  
+  }
+
+  
 }
